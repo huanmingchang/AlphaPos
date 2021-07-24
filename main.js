4e88771cd78a77cd35771a605077665a87ca6578
huanmingchang <huanming1988@gmail.com>
2021-07-24 11:24:56 +0800

    feat: add adding order function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
+const alphaPos = new AlphaPos()
+const orderLists = document.querySelector('[data-order-lists]')
+
 function Drink(name, sugar, ice) {
   this.name = name
   this.sugar = sugar
@@ -21,3 +25,49 @@ Drink.prototype.price = function () {
       alert('No this drink')
   }
 }
+
+function AlphaPos() {}
+
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectOption = ''
+  document.querySelectorAll(`[name="${inputName}"]`).forEach(function (item) {
+    if (item.checked) {
+      selectOption = item.value
+    }
+  })
+  return selectOption
+}
+
+AlphaPos.prototype.addDrink = function (drink) {
+  let orderListsCard = `
+    <div class="card mb-3">
+    <div class="card-body pt-3 pr-3">
+      <div class="text-right">
+        <span data-alpha-pos="delete-drink">×</span>
+      </div>
+      <h6 class="card-title mb-1">${drink.name}</h6>
+      <div class="card-text">${drink.ice}</div>
+      <div class="card-text">${drink.sugar}</div>
+    </div>
+    <div class="card-footer text-right py-2">
+      <div class="card-text text-muted">$ <span data-drink-price>${drink.price()}</span></div>
+    </div>
+  </div>
+  `
+
+  orderLists.insertAdjacentHTML('afterbegin', orderListsCard)
+}
+
+addDrinkButton.addEventListener('click', function () {
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+
+  if (!drinkName) {
+    alert('Please choose at least one item.')
+    return
+  }
+
+  const drink = new Drink(drinkName, sugar, ice)
+  alphaPos.addDrink(drink)
+})
